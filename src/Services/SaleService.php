c1555c3a1a2e151f12ee59ea2306aa8e080f5931
wordsword-galaxy <vladimir.g@galaxy-it.net>
2023-05-30 14:01:47 +0300

    fixed sale auth

--- src/Services/SaleService.php
+++ src/Services/SaleService.php
@@ -10,16 +10,17 @@ class SaleService
     protected Config $config;
     protected AuthService $auth;
 
-    public function __construct(XmlService $xmlService, Config $config)
+    public function __construct(XmlService $xmlService, Config $config, AuthService $authService)
     {
         $this->xml = $xmlService;
         $this->config = $config;
+        $this->auth = $authService;
     }
 
     public function query()
     {
         $this->auth->auth();
-        
+
         $path = $this->config->getFullPath('orders.xml');
 
         $data = file_exists($path) ? file_get_contents($path) : [];
